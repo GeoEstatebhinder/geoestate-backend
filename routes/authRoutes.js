389d8f559629795a222aeccbf3ffdd48f5ea49f3
Bhinder Singh <bhinder@example.com>
2025-08-07 10:15:53 +0530

    Fix login route and auth system

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
-const User = require('../models/userModel');
+const User = require('../models/user');
 
 // ✅ POST /api/auth/register
 router.post('/register', async (req, res) => {
